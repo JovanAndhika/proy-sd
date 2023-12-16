d08b676cf15a2eccc3dcdd255f6cc48b39b50755
terrGit <terrenceoscarjehuda@gmail.com>
2023-12-16 19:33:03 +0700

    semangat

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ class Queue {
   enqueue(item) {
       this.items[this.backIndex] = item
       this.backIndex++
-      return item + ' inserted'
   }
   dequeue() {
       const item = this.items[this.frontIndex]
@@ -19,57 +18,51 @@ class Queue {
   peek() {
       return this.items[this.frontIndex]
   }
-  get printQueue() {
-      return this.items;
-  }
 }
+
 const queueRow = new Queue();
 const queueCol = new Queue();
 
-
-
-//BUAT GRAPH
-var noAccess = false;
+// Graph matrix representing the game board
 var graphMatrix = [
-                    [0,1,1,0,0,0,0,0,0],
-                    [1,0,0,1,0,0,0,0,0],
-                    [1,0,0,1,1,0,0,0,0],
-                    [0,1,1,0,0,1,0,0,0],
-                    [0,0,1,0,0,1,1,0,0],
-                    [0,0,0,1,1,0,0,1,0],
-                    [0,0,0,0,1,0,0,1,1],
-                    [0,0,0,0,0,1,1,0,0],
-                    [0,0,0,0,0,0,1,0,0],
-                  ];
+    [0,1,1,0,0,0,0,0,0],
+    [1,0,0,1,0,0,0,0,0],
+    [1,0,0,1,1,0,0,0,0],
+    [0,1,1,0,0,1,0,0,0],
+    [0,0,1,0,0,1,1,0,0],
+    [0,0,0,1,1,0,0,1,0],
+    [0,0,0,0,1,0,0,1,1],
+    [0,0,0,0,0,1,1,0,0],
+    [0,0,0,0,0,0,1,0,0],
+  ];
 var temp = graphMatrix;
 
 var visited = [
-  [false,false,false,false,false,false,false,false,false],
-  [false,false,false,false,false,false,false,false,false],
-  [false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false],
 
-  [false,false,false,false,false,false,false,false,false],
-  [false,false,false,false,false,false,false,false,false],
-  [false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false],
 
-  [false,false,false,false,false,false,false,false,false],
-  [false,false,false,false,false,false,false,false,false],
-  [false,false,false,false,false,false,false,false,false]
+[false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false],
+[false,false,false,false,false,false,false,false,false]
 ];
 
-
-//DESKRIPSI GAME
 var max_block = 5;
 var max_zombieEnergy = 9;
-var rowPosition = [-1, 1, 0, 0]
+var zombieRow = 0;  // Starting row for the zombie
+var zombieCol = 0;  // Starting column for the zombie
+var endRow = 8;     // Row for the end button
+var endCol = 8;     // Column for the end button
+var rowPosition = [-1, 1, 0, 0];
 var columnPosition = [0, 0, 1, -1];
 
 function startGame(){
-
-
+    solve(zombieRow, zombieCol);
 }
-
-
 
 function clickNode(clicked_id){
   let count = 0;
@@ -82,24 +75,73 @@ function clickNode(clicked_id){
       count += 1;
     }
   }
-  djikstra();
+ 
 }
 
-
-function djikstra(){
-
+function isSafe(row, col) {
+    return (row >= 0) && (row < graphMatrix.length) && 
+           (col >= 0) && (col < graphMatrix[row].length) &&
+           graphMatrix[row][col] === 1;
 }
 
+function solve(startRow, startColumn) {
+    queueRow.enqueue(startRow);
+    queueCol.enqueue(startColumn);
+    visited[startRow][startColumn] = true;
 
-function checkPosition(){
+    while (queueRow.peek() !== undefined && max_zombieEnergy > 0) {
+        let currRow = queueRow.dequeue();
+        let currCol = queueCol.dequeue();
+        max_zombieEnergy--;
 
+        // Check if zombie reached the end
+        if (currRow === endRow && currCol === endCol) {
+            alert('Zombie wins!');
+            return;
+        }
+
+        // Check adjacent nodes
+        for (let i = 0; i < 4; i++) {
+            let nextRow = currRow + rowPosition[i];
+            let nextCol = currCol + columnPosition[i];
+
+            if (isSafe(nextRow, nextCol) && !visited[nextRow][nextCol]) {
+                queueRow.enqueue(nextRow);
+                queueCol.enqueue(nextCol);
+                visited[nextRow][nextCol] = true;
+            }
+        }
+    }
+
+    // Check if zombie energy depleted
+    if (max_zombieEnergy <= 0) {
+        alert('Player wins!');
+    }
 }
 
-
-function solve(startRow, startColumn){
-  queueRow.enqueue(startRow);
-  queueCol.enqueue(startColumn);
-  visited[startRow][startColumn] = true;
+function executeMove() {
+    // This function should be called each time the execute button is pressed
+    if (queueRow.peek() !== undefined && max_zombieEnergy > 0) {
+        let nextRow = queueRow.peek();
+        let nextCol = queueCol.peek();
+        moveZombieTo(nextRow, nextCol);
+    } else {
+        alert('No more moves possible!');
+    }
 }
 
+function moveZombieTo(row, col) {
+    // Logic to visually move the zombie to the new position
+    // Update the zombie's current position
+    zombieRow = row;
+    zombieCol = col;
+    solve(zombieRow, zombieCol);
+}
 
+function cek(){
+    let nextRow = queueRow.peek();
+        let nextCol = queueCol.peek();
+    alert(nextRow );
+}
+
+// Add any additional functions or logic as needed
