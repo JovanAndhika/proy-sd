56d7a84ac37cdd1706947cb55c9f211d0de86fe7
Jovan Andhika <c14220013@john.petra.ac.id>
2023-12-17 11:10:32 +0700

    ver 1.4

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class Queue {
   constructor() {
-    this.items = {};
+    this.items = [];
     this.frontIndex = 0;
     this.backIndex = 0;
   }
@@ -27,65 +27,8 @@ class Queue {
     return this.items;
   }
 }
-const queue = new Queue();
+const qu = new Queue();
 
-class Cell {
-  constructor(x, y, distance, prev) {
-    this.x = x;
-    this.y = y;
-    this.distance = distance;
-    this.prev = prev; //parent cell (last visited cell)
-  }
-}
-
-class ShortestPathBetweenCellsBFS {
-
-
-  shortestPath(graphAdjen, startRow, startColumn, endRow, endColumn) {
-    var start_r = startRow;
-    var start_c = startColumn;
-    var desti_r = endRow;
-    var desti_c = endColumn;
-
-
-    //JIKA ADJENCY MATRIX ADALAH 0
-    if (
-      graphAdjen[startRow][startColumn] == 0 ||
-      graphAdjen[endRow][endColumn] == 0
-    ) {
-      alert("There is no path.");
-      return;
-    }
-
-    
-    var visited = [];
-    for (let i = 0; i < graphAdjen.length; i++) {
-      visited[i] = [];
-      for (let j = 0; j < graphAdjen[i].length; j++) {
-        if (graphAdjen[i][j] != 0) {
-          visited[i][j] = new Cell(i, j, Number.MAX_VALUE, null);
-        }
-      }
-    }
-
-    
-    //BFS
-    var search = visited[sx][sy];
-    search.distance = 0;
-    queue.enqueue(src);
-
-    var dest = null;
-    var p = queue.dequeue();
-    while (p != null) {
-
-      //find destination
-      if (p.x == dx && p.y == dy) {
-        dest = p;
-        break;
-      }
-    }
-  }
-}
 //BUAT GRAPH
 const graphNumber = [
   [0, 1, 2, 3, 4, 5, 6, 7, 8],
@@ -101,50 +44,159 @@ const graphNumber = [
 ];
 
 //BUAT EDGES GRAPH
-const graphAdjen = [
-  [0, 1, 1, 0, 0, 0, 0, 0, 0],
-  [1, 0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 1, 0, 0, 0, 0],
-  [0, 1, 1, 0, 0, 1, 0, 0, 0],
-  [0, 0, 1, 0, 0, 1, 1, 0, 0],
-  [0, 0, 0, 1, 1, 0, 0, 1, 0],
-  [0, 0, 0, 0, 1, "E", 0, 1, 1],
-  [0, 0, 0, 0, 0, 1, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+// const graphAdjen = [
+//   [0, 1, 1, 0, 0, 0, 0, 0, 0],
+//   [1, 0, 0, 1, 0, 0, 0, 0, 0],
+//   [1, 0, 0, 1, 1, 0, 0, 0, 0],
+//   [0, 1, 1, 0, 0, 1, 0, 0, 0],
+//   [0, 0, 1, 0, 0, 1, 1, 0, 0],
+//   [0, 0, 0, 1, 1, 0, 0, 1, 0],
+//   [0, 0, 0, 0, 1, 0, 0, 1, 1],
+//   [0, 0, 0, 0, 0, 1, 1, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 1, 0, 0],
+// ];
+// var tempAdjent = graphAdjen;
+
+
+var tiles = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
-var tempAdjent = graphAdjen;
 
 //DESKRIPSI GAME
 var max_block = 5;
 var max_zombieEnergy = 9;
-var rowPosition = [-1, 1, 0, 0];
-var columnPosition = [0, 0, 1, -1];
 
-function startGame() {
-  solve(6, 0);
+class Cell {
+  constructor(x, y, dist, prev) {
+    this.x = x;
+    this.y = y;
+    this.dist = dist; //distance
+    this.prev = prev; //parent cell in the path
+  }
+  toString() {
+    return "(" + this.x + ", " + this.y + ")";
+  }
 }
 
-function refreshGame() {
-  graphAdjen = tempAdjent;
-}
+class ShortestPathBetweenCellsBFS {
+  //BFS, Time O(n^2), Space O(n^2)
+  shortestPath(tiles, start, end) {
+    var sx = start[0];
+    var sy = start[1];
+    var dx = end[0];
+    var dy = end[1];
+    if (matrix[sx][sy] == 0 || matrix[dx][dy] == 0) {
+      console.log("There is no path.");
+      return;
+    }
 
-//FUNCTION clickNode
-var countTile = 0;
 
-function clickNode(clicked_id) {
-  let countID = 0;
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < graphNumber[i].length; j++) {
-      if (countID == clicked_id) {
-        if (countTile == max_block) {
-          alert("max_block tercapai");
-        } else {
-          document.getElementById(clicked_id).style.backgroundColor = "red";
-          graphAdjen[i][j] = 0;
-          countTile += 1;
+    //initialize the cells
+    var m = matrix.length;
+    var n = matrix[0].length;
+    var cells = []; //visited
+    for (let i = 0; i < m; i++) {
+      cells[i] = [];
+      for (let j = 0; j < n; j++) {
+        if (matrix[i][j] != 0) {
+          cells[i][j] = new Cell(i, j, Number.MAX_VALUE, null);
         }
       }
-      countID += 1;
+    }
+    //breadth first search
+    var queue = [];
+    var src = cells[sx][sy];
+    src.dist = 0;
+    queue.push(src);
+    var dest = null;
+    var p;
+    while ((p = queue.shift()) != null) {
+      //find destination
+      if (p.x == dx && p.y == dy) {
+        dest = p;
+        break;
+      }
+      // moving up
+      this.visit(cells, queue, p.x - 1, p.y, p);
+      // moving left
+      this.visit(cells, queue, p.x, p.y - 1, p);
+      // moving down
+      this.visit(cells, queue, p.x + 1, p.y, p);
+      //moving right
+      this.visit(cells, queue, p.x, p.y + 1, p);
+    }
+
+    if (dest == null) {
+      console.log("there is no path.");
+      return;
+    } else {
+      let path = [];
+      p = dest;
+      do {
+        path.unshift(p);
+      } while ((p = p.prev) != null);
+      console.log(`${path}`);
+    }
+  }
+
+  visit(cells, queue, x, y, parent) {
+    //out of boundary
+    if (
+      x < 0 ||
+      x >= cells.length ||
+      y < 0 ||
+      y >= cells[0].length ||
+      cells[x][y] == null
+    ) {
+      return;
+    }
+
+    //update distance , prev node
+    var dist = parent.dist + 1;
+    var p = cells[x][y];
+    if (dist < p.dist) {
+      p.dist = dist;
+      p.prev = parent;
+      queue.push(p);
     }
   }
 }
+
+var countTile = 0;
+function clickNode(id){
+  let countId = 0;
+  for(let i = 0; i < graphNumber.length; i++){
+    for(let j = 0; j < graphNumber[i].length; j++){
+      if(countId == id){
+
+        if(countTile == max_block){
+          alert('maximum tile selected');
+          return;
+        }
+
+        document.getElementById(id).style.backgroundColor = 'red';
+        tiles[i][j] = 0;
+        countTile += 1;
+      }
+  
+      countId += 1;
+    }
+  }
+}
+
+
+myObj = new ShortestPathBetweenCellsBFS();
+
+//find path
+let start1 = [0, 2];
+let end1 = [6, 6];
+console.log("case: ");
+myObj.shortestPath(tiles, start1, end1);
